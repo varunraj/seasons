c4c3943dfc774e204e11f573990f41d578cab662
Varun Raj <varunraj82@gmail.com>
2020-07-19 23:42:58 -0400

    updated to class compoent

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { lat: null, errorMessage:''};
+        
+        window.navigator.geolocation.getCurrentPosition(
+            
+            (position)=>{
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err)=>{
+                this.setState({ errorMessage: err.message });
+            }
+        )
+    
+    }
+
+    /*
+
+    componentDidMount(){
+        console.log("Component rendered to the screen");
+    }
+
+    componentDidUpdate(){
+        console.log("Component got updated")
+    }
+
+    */
+
+    render(){
+        
+            if (this.state.errorMessage && !this.state.lat){
+                return <div>Error: {this.state.errorMessage}</div>;
+            }
+
+            if (!this.state.errMessage && this.state.lat){
+                return <div>Latitude: {this.state.lat}</div>;
+            }
+
+           return <div>Loading !!!!</div>;
+        
+    }
+
+}
+
+
+reactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
+
+
+
